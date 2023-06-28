478e1d5224a31030e0943fc5c3ccff60f12ca6c1
criscop3 <criscop3@yahoo.com>
2023-06-28 02:10:33 -0400

    completed challenge

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "a72cf74f7fb9c137c131f1516ebd4a9d"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "8ef0514bd9184d3898bf0b12468f7452"
